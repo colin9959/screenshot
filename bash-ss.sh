564b969f76830789889ed10d4c1e01d0e8329d41
colinshu <45537167+colin9959@users.noreply.github.com>
2026-05-06 15:03:20 +0800

    Update bash-ss.sh

--- bash-ss.sh
+++ bash-ss.sh
@@ -36,18 +36,20 @@ sudo chmod +x /usr/local/bin/nconvert
 # 6. 下载并安装 BDInfoCLI-ng（zip版）
 echo -e "\n[6/7] 下载并安装 BDInfoCLI-ng..."
 sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/BDInfoCLI-ng-main.zip -o /home/BDInfoCLI-ng-main.zip
+sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/bd.sh -o /usr/local/bin/bd.sh
 sudo unzip -o /home/BDInfoCLI-ng-main.zip -d /home/
 sudo rm -f /home/BDInfoCLI-ng-main.zip
 sudo chmod +x /home/BDInfoCLI-ng-main/scripts/bdinfo
 sudo cp /home/BDInfoCLI-ng-main/scripts/bdinfo /usr/local/bin/
 sudo chmod +x /usr/local/bin/bdinfo
+sudo chmod +x /usr/local/bin/bd.sh
 # 7. 安装完成提示
 echo -e "\n========================================"
 echo "           所有操作执行完成！"
 echo ""
-echo " ✅ ss.sh        全局命令：ss.sh"
+echo " ✅ screenshot   全局命令：ss.sh"
 echo " ✅ nconvert     全局命令：nconvert"
-echo " ✅ bdinfo       全局命令：bdinfo"
+echo " ✅ bdinfo       全局命令：bd.sh"
 echo ""
 echo " 三个工具已全部安装成功！"
 echo "========================================"
