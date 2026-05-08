399c48d0c9f8f1595d87f3c095093aa32c8f2195
colinshu <45537167+colin9959@users.noreply.github.com>
2026-05-08 17:03:24 +0800

    Update bash-ss.sh

--- arm/bash-ss.sh
+++ arm/bash-ss.sh
@@ -7,16 +7,16 @@ echo "        支持：ss截图 + BDInfo蓝光检测"
 echo "========================================"
 #
 # 1. 创建ISO挂载目录
-echo -e "\n[3/7] 创建ISO挂载目录 /mnt/iso..."
+echo -e "\n[1/5] 创建ISO挂载目录 /mnt/iso..."
 sudo mkdir -p /mnt/iso
 # 2. 下载 ss.sh 和 nconvert
-echo -e "\n[4/7] 下载 ss.sh文件..."
+echo -e "\n[2/5] 下载 ss.sh文件..."
 sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/arm/ss.sh -o /usr/local/bin/ss.sh
 # 3. 赋予执行权限
-echo -e "\n[5/7] 赋予文件执行权限..."
+echo -e "\n[3/5] 赋予文件执行权限..."
 sudo chmod +x /usr/local/bin/ss.sh
 # 4. 下载并安装 BDInfo（arm版）
-echo -e "\n[6/7] 下载并安装 BDInfo..."
+echo -e "\n[4/5] 下载并安装 BDInfo..."
 sudo curl -fsSL https://github.com/colin9959/screenshot/releases/download/arm/bdinfo -o /usr/local/bin/bdinfo
 sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/arm/bd.sh -o /usr/local/bin/bd.sh
 sudo chmod +x /usr/local/bin/bdinfo
