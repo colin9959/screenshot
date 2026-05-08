4adc044577f9f17cf58e9cc1114940f3b235c675
colinshu <45537167+colin9959@users.noreply.github.com>
2026-05-08 18:50:43 +0800

    Update bash-ss.sh

--- arm/bash-ss.sh
+++ arm/bash-ss.sh
@@ -15,11 +15,9 @@ sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/arm/
 # 3. 赋予执行权限
 echo -e "\n[3/5] 赋予文件执行权限..."
 sudo chmod +x /usr/local/bin/ss.sh
-# 4. 下载并安装 BDInfo（arm版）
+# 4. 下载 bd.sh文件（arm版）
 echo -e "\n[4/5] 下载并安装 BDInfo..."
-sudo curl -fsSL https://github.com/colin9959/screenshot/releases/download/arm/bdinfo -o /usr/local/bin/bdinfo
 sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/arm/bd.sh -o /usr/local/bin/bd.sh
-sudo chmod +x /usr/local/bin/bdinfo
 sudo chmod +x /usr/local/bin/bd.sh
 # 5. 安装完成提示
 echo -e "\n========================================"
