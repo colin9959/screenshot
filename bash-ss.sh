9a4d01a5d0c52ca2bac278670e0ba77b8d45e75f
colinshu <45537167+colin9959@users.noreply.github.com>
2026-05-08 22:11:59 +0800

    Update bash-ss.sh

--- bash-ss.sh
+++ bash-ss.sh
@@ -23,7 +23,7 @@ sudo chmod +x /usr/local/bin/nconvert
 # 4. 下载并安装 BDInfoCLI-ng（zip版）
 echo -e "\n[4/5] 下载并安装 BDInfoCLI-ng..."
 sudo curl -fsSL https://raw.githubusercontent.com/colin9959/screenshot/main/BDInfoCLI-ng-main.zip -o /home/BDInfoCLI-ng-main.zip
-@@ -25,6 +30,7 @@ sudo chmod +x /home/BDInfoCLI-ng-main/scripts/bdinfo
+sudo chmod +x /home/BDInfoCLI-ng-main/scripts/bdinfo
 sudo cp /home/BDInfoCLI-ng-main/scripts/bdinfo /usr/local/bin/
 sudo chmod +x /usr/local/bin/bdinfo
 sudo chmod +x /usr/local/bin/bd.sh
